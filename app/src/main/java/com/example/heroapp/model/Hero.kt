53e313ed8d1ba06375d0ed99d1ade86bf6754fd5
gyotro <giovanni.dintrono@gmail.com>
2023-05-02 19:24:19 +0200

    DB Data Class added

---
+++ app/src/main/java/com/example/heroapp/model/Hero.kt
@@ -0,0 +1,21 @@
+package com.example.heroapp.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.heroapp.util.Constants.HERO_DB
+
+@Entity(tableName = HERO_DB)
+data class Hero(
+    @PrimaryKey(autoGenerate = false) // noi già abbiamo l'Id
+    val id: Int,
+    val name: String,
+    val image: String,
+    val about: String,
+    val rating: Double,
+    val power: Int,
+    val month: String,
+    val day: String,
+    val family: List<String>,
+    val abilities: List<String>,
+    val natureTypes: List<String>
+)
--- app/src/main/java/com/example/heroapp/util/Constants.kt
+++ app/src/main/java/com/example/heroapp/util/Constants.kt
@@ -3,4 +3,5 @@ package com.example.heroapp.util
 object Constants {
     // evitiamo valori hardCoded, usiamo invece i constant object
     const val DETAILS_ARGUMENT_KEY = "heroId"
+    const val HERO_DB = "hero_table"
 }
