71ba3744d4597d9d2e12c9cb874466eb9f0f4735
Giovanni D'Introno <giovanni.dintrono@gmail.com>
2023-08-22 12:15:39 +0200

    OrderedList Added

---
+++ app/src/main/java/com/example/heroapp/presentation/components/InfoBox.kt
@@ -0,0 +1,77 @@
+package com.example.heroapp.presentation.components
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material3.Text
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.heroapp.R
+import com.example.heroapp.ui.theme.SMALL_PADDING
+
+
+@Composable
+fun InfoBox(
+    icon: Painter,
+    iconColor: Color,
+    bigText: String,
+    smallText: String,
+    textColor: Color
+    ) {
+    Row(
+        // se nn lo mettiamo, non ci allinea l'icona al testo
+        verticalAlignment = Alignment.CenterVertically
+    ){
+       Icon(
+           modifier = Modifier.padding(end = SMALL_PADDING).size(40.dp),
+           painter = icon,
+           contentDescription = "Info Icon",
+           tint = iconColor
+       )
+        Column {
+            Text(
+                text = bigText,
+                color = textColor,
+                fontSize = MaterialTheme.typography.headlineSmall.fontSize,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                modifier = Modifier.alpha(ContentAlpha.medium),
+                text = smallText,
+                color = textColor,
+                fontSize = MaterialTheme.typography.headlineSmall.fontSize,
+                fontWeight = FontWeight.Light
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun PreviewInfoBox() {
+    InfoBox(
+        icon = painterResource(id = R.drawable.electric_bolt),
+        iconColor = MaterialTheme.colorScheme.primary,
+        bigText = "92",
+        smallText = "Power",
+        textColor = if (isSystemInDarkTheme()) Color.LightGray else Color.DarkGray
+    )
+}
---
+++ app/src/main/java/com/example/heroapp/presentation/components/OrderedList.kt
@@ -0,0 +1,63 @@
+package com.example.heroapp.presentation.components
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.icu.text.CaseMap.Title
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun OrderedList(
+    title: String,
+    items: List<String>,
+    textColor: Color
+){
+    Column(horizontalAlignment = Alignment.Start) {
+        Text(
+            text = title,
+            color = textColor,
+            fontSize = MaterialTheme.typography.headlineSmall.fontSize,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.padding(10.dp))
+        items.forEachIndexed { index, item ->
+            Text(
+                modifier = Modifier.alpha(ContentAlpha.medium),
+                text = "${index + 1}. $item",
+                color = textColor,
+                fontSize = MaterialTheme.typography.headlineSmall.fontSize,
+                fontWeight = FontWeight.Light
+            )
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun OrderedListPreview() {
+    OrderedList(
+        title = "Family",
+        items = listOf("Hero1", "Hero2"),
+        textColor = if (isSystemInDarkTheme()) Color.LightGray else Color.DarkGray
+    )
+}
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun OrderedListPreviewDark() {
+    OrderedList(
+        title = "Family",
+        items = listOf("Hero1", "Hero2"),
+        textColor = if (isSystemInDarkTheme()) Color.LightGray else Color.DarkGray
+    )
+}
